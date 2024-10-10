54c90dab0ec494b944739a7eded5342e5ddf5607
willenius <wilhelm.bjor@gmail.com>
2024-10-10 13:23:32 +0200

    uppdaterat

--- quiz/script.js
+++ quiz/script.js
@@ -1,4 +1,5 @@
-//These are all of my questions which are stored in arrays.
+//These are all of my questions which are stored in arrays. This is a manually created quiz.
+
 const quizQuestions = [
     {
         question: "Which nation won the UEFA Euros 1992 despite not even qualifying?",
@@ -25,14 +26,40 @@ const quizQuestions = [
 
     {
         question: ["Which goalkeeper ended up playing ice-hockey after his carreer?"],
-        options: ["Petr Čech", "Iker Casillas", "Gianluigi Buffon","Edvin van Der Sar" ],
+        options: ["Petr Čech", "Iker Casillas", "Gianluigi Buffon", "Edvin van Der Sar"],
         correctAnswer: "Petr Čech decided to follow his boy dream of becoming a professional hockey goaltender in England.",
 
     }
 
 ];
 
-//Mostly too see if my arrays are working.
-console.log(quizQuestions);
+//here's my function which randomizes all my quiz-questions.
+function randomQuestion() {
+    console.log(quizQuestions[(Math.floor(Math.random() * quizQuestions.length))]);
+}
 
-fetch('https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple');
+const quizContainer = document.getElementById("quizContainer"); //fetching existing div
+const nextQuestionDiv = document.createElement("div"); //here i create a new element for a new question
+
+
+//const randomQuestion = quizQuestions[Math.floor(math.random() * quizQuestions.length)];
+
+newQuestionDiv.textContent = randomQuestion.question;
+quizContainer.appendChild(newQuestionDiv);
+
+//Nästa steg, displaya frågorna från html --> JS och in i chrome.
+
+const nextBtn = document.getElementById("nextBtn");
+nextBtn.addEventListener('click', function (event){
+    alert("Start Quiz")
+})
+
+console.log();
+
+
+
+
+
+//Mostly too see if my arrays are working.
+//console.log(quizQuestions);
+//fetch('https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple');
